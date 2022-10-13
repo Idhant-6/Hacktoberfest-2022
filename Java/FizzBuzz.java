b8eb45f0bb3259866f70b90e851a14cbdfc9d7d9
David <87286155+kindadumbdave@users.noreply.github.com>
2022-10-13 09:23:38 +0200

    Update FizzBuzz.java
    Add comments to explain the challenge.

--- Java/FizzBuzz.java
+++ Java/FizzBuzz.java
@@ -1,4 +1,9 @@
-class FizzBuzz {
+/* 
+A common interview question/challenge is the FizzBuzz test.
+The Test: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+*/
+
+public class FizzBuzz {
 
   public static void main(String[] args) {
     for(int i=1; i<=100; i++){
